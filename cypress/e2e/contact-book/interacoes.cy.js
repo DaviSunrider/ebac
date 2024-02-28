57cff151a6c40e08083be83fa16573afde29d866
DaviSunrider <115567218+DaviSunrider@users.noreply.github.com>
2024-02-28 13:13:29 -0300

    correção de exclusão

--- cypress/e2e/contact-book/interacoes.cy.js
+++ cypress/e2e/contact-book/interacoes.cy.js
@@ -1,36 +1,42 @@
-beforeEach(()=>{
-  cy.visit('https://agenda-contatos-react.vercel.app/')
-})
-
-
-describe('testar inclusão de item', () => {
-  it('deve verificar a inclusão de itens 1', () => {
-    cy.get('input[type="text"]').type('gian Souza');
-    cy.get('input[type="tel"]').type('11912345678');
-    cy.get('input[type="email"]').type('gian@ebac.com.br');
-    cy.get('button[type="submit"]').click();
-
-    cy.get('input[type="text"]').type('Gian');
-    cy.get('input[type="tel"]').type('117878755454');
-    cy.get('input[type="email"]').type('gianprofessor@ebac.com.br');
-    cy.get('button[type="submit"]').click();
-
-    cy.get('.contato').should('have.length.gt', 1); 
-  })
-
-  it('deve verificar a exclusao de um item', () => {
-    cy.get('.contato:first .delete').click();
-
-  })
+beforeEach(() => {
+  cy.visit("https://agenda-contatos-react.vercel.app/");
 });
-describe('testar edição de item', () => {
-it('deve verificar a edição de itens', () => {
 
-  cy.get('.contato:eq(1) .edit').click();
-  cy.get('input[type="text"]').clear().type('Novo Nome');
-  cy.get('input[type="tel"]').clear().type('999999999');
-  cy.get('input[type="email"]').clear().type('novo@email.com');
-  cy.get('.alterar').click();
+describe("testar inclusão de item", () => {
+  it("deve verificar a inclusão de itens 1", () => {
+    cy.get('input[type="text"]').type("gian Souza");
+    cy.get('input[type="tel"]').type("11912345678");
+    cy.get('input[type="email"]').type("gian@ebac.com.br");
+    cy.get('button[type="submit"]').click();
 
-})
-})
+    cy.get('input[type="text"]').type("Gian");
+    cy.get('input[type="tel"]').type("117878755454");
+    cy.get('input[type="email"]').type("gianprofessor@ebac.com.br");
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".contato").should("have.length.gt", 1);
+  });
+
+  it("deve verificar a exclusao de um item", () => {
+    cy.get(".contato")
+      .its("length")
+      .then((originalCount) => {
+        cy.get(".contato:first .delete").click();
+        cy.get(".contato").should("have.length", originalCount - 1);
+      });
+  });
+});
+
+describe("testar edição de item", () => {
+  it("deve verificar a edição de itens", () => {
+    cy.get(".contato:eq(1) .edit").click();
+    cy.get('input[type="text"]').clear().type("Novo Nome");
+    cy.get('input[type="tel"]').clear().type("999999999");
+    cy.get('input[type="email"]').clear().type("novo@email.com");
+    cy.get(".alterar").click();
+
+    cy.get(".contato:eq(1)").should("contain", "Novo Nome");
+    cy.get(".contato:eq(1)").should("contain", "999999999");
+    cy.get(".contato:eq(1)").should("contain", "novo@email.com");
+  });
+});
