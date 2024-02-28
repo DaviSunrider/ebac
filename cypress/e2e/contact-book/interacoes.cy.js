9c389bc62bd33191be81aba989bf1f63e0d14a6c
DaviSunrider <115567218+DaviSunrider@users.noreply.github.com>
2024-02-27 21:12:41 -0300

    exercicio crypress

---
+++ cypress/e2e/contact-book/interacoes.cy.js
@@ -0,0 +1,36 @@
+beforeEach(()=>{
+  cy.visit('https://agenda-contatos-react.vercel.app/')
+})
+
+
+describe('testar inclusão de item', () => {
+  it('deve verificar a inclusão de itens 1', () => {
+    cy.get('input[type="text"]').type('gian Souza');
+    cy.get('input[type="tel"]').type('11912345678');
+    cy.get('input[type="email"]').type('gian@ebac.com.br');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('input[type="text"]').type('Gian');
+    cy.get('input[type="tel"]').type('117878755454');
+    cy.get('input[type="email"]').type('gianprofessor@ebac.com.br');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.contato').should('have.length.gt', 1); 
+  })
+
+  it('deve verificar a exclusao de um item', () => {
+    cy.get('.contato:first .delete').click();
+
+  })
+});
+describe('testar edição de item', () => {
+it('deve verificar a edição de itens', () => {
+
+  cy.get('.contato:eq(1) .edit').click();
+  cy.get('input[type="text"]').clear().type('Novo Nome');
+  cy.get('input[type="tel"]').clear().type('999999999');
+  cy.get('input[type="email"]').clear().type('novo@email.com');
+  cy.get('.alterar').click();
+
+})
+})
